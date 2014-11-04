c2697e536e2559b7d7a06bbdfc7014dd1eb6b3bc
marchdown <marchdown@gmail.com>
2014-11-05 01:26:06 +0300

    test suite

--- codewars/look_and_say_test.py
+++ codewars/look_and_say_test.py
@@ -1,27 +1,38 @@
 import unittest
 from look_and_say import *
-class look_and_say_test(unittest.TestCase):
-	def test_first_iteration(self):
+class Test_make_next_look_and_say_number(unittest.TestCase):
+	def runTest(self):
 		self.assertEqual('11',
 			make_next_look_and_say_number('1'))
-	def test_third_iteration(self):
+
 		self.assertEqual('1211',
 			make_next_look_and_say_number('21'))		
 
-	def test_list(self):
+class Test_look_and_say(unittest.TestCase):
+	def runTest(self):
 		self.assertEqual(look_and_say(maxlen=10),
 		 ['1', '11', '21', '1211', '111221', '312211', '13112221', '1113213211'])
 
-	def test_zero(self):
+
 		self.assertEqual(look_and_say(maxlen=0),
 		 [])
-	def test_one(self):
+
 		self.assertEqual(look_and_say(maxlen=1),
 		 ['1'])	
-	def test_100500(self):
-		self.assertEqual(len(look_and_say(maxlen=100500)),
-		 41)				 	
 
+		self.assertEqual(len(look_and_say(maxlen=100500)),
+		 41)				 
+def suite():
+	suite = unittest.TestSuite()
+	suite.addTest(Test_look_and_say())
+	suite.addTest(Test_make_next_look_and_say_number())
+	return suite
 
 if __name__ == '__main__':
+	# suite = suite()
+	# runner = unittest.TextTestRunner()
+	# runner.run(suite)
 	unittest.main()
+
+# http://blogs.wrox.com/article/python-test-cases-and-test-suites/
+
