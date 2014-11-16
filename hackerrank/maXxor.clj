faeef83e10ee9c905c4652808b1b1a7ec7d1520e
marchdown <marchdown@gmail.com>
2014-11-16 02:08:26 +0000

    hackerrank #3

--- hackerrank/maXxor.clj
+++ hackerrank/maXxor.clj
@@ -2,12 +2,15 @@
 ;;(defn abs [x] (if (neg? x) (- x) x))
 (defn maxxor [m n]
   (if
-    (partition-by identity (map binlog '(m n)))
+    (not= (binlog m) (binlog n))
     (Math/abs (- m n))
-    (dec(Math/pow 2 (inc(binlog n))))))
+    (dec(Math/pow 2 (binlog n)))))
 
 ;;(map binlog (range  2 12))
 ;;(dec (Math/pow 2 (binlog 3)))
 ;;(maxxor 10 15)
 
 ;;  (max(map binlog '(9 12)))
+(binlog 15)
+(maxxor 10 15)
+(maxxor 10 12)
