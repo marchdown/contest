9330d00fe4ab8882f5db4031d8195e44dd4b5821
marchdown <marchdown@gmail.com>
2014-11-19 11:35:05 +0000

    Mind the 0. Accepted.

--- hackerrank/find-digits/src/find_digits/core.clj
+++ hackerrank/find-digits/src/find_digits/core.clj
@@ -1,14 +1,11 @@
 (defn -main [] 
-  (let [T (Integer/parseInt (read-line))]
-    (loop [t T]
-      (when (pos? t) ;; stop condition
-        (let [s (read-line) n (Integer/parseInt s)]  ;; read from STDIN and bind
-          (println 
-           (count (filter 
-                   #(zero? (mod n (Integer/parseInt %)))
-                   (filter
-                    #(not= "" %)
-                    (clojure.string/split s #"")))))) 
-        (recur (dec t))) ;; loopity-loop.
-      )))
+  (loop [t (Integer/parseInt (read-line))]
+    (when (pos? t)
+      (let [s (read-line) n (Integer/parseInt s)]
+        (println 
+         (count (filter 
+                 #(zero? (mod n (Integer/parseInt %)))
+                 (filter  #(and (not= "0" %) ( not= "" %))
+                  (clojure.string/split s #"")))))) 
+      (recur (dec t)))))
 (-main)
