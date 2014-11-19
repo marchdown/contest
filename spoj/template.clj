8df466279198b9d50937e08bc5ea11a639e20187
marchdown <marchdown@gmail.com>
2014-11-19 10:47:07 +0000

    parse numbers

--- spoj/template.clj
+++ spoj/template.clj
@@ -1,9 +1,15 @@
 ;; get input
+(defn parse-string-into-ints [s]
+  (map #(Integer/parseInt %) (clojure.string/split s #"\s")))
+(defn parse-string-into-doubles [s]
+  (map #(Double/parseDouble %) (clojure.string/split s #"\s")))
+
 (defn change-t [t] (dec t))
 (defn do-stuff [t] )
 (defn should-stop? [t] (neg? (dec t)))
 (loop [t (Integer/parseInt (readline)] ;; t ← an int from STDIN
        (when-not (should-stop? t)
-         (do-stuff t)
+         (let [s (read-line)]
+           (do-stuff s))
          (recur (change-t t)))
 ))
