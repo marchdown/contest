69cd2d957f6d9e540c1ad9396a5b5ba9dfc45977
marchdown <marchdown@gmail.com>
2014-12-05 12:30:42 +0000

    format is fine; not sure if line-by-line processing is OK

--- hackerrank/love_letter_mystery.hs
+++ hackerrank/love_letter_mystery.hs
@@ -1,13 +1,18 @@
 import Data.Char
--- main = do
---   t <- readLn -- :: Int
---   ws <- [readLn | n <- [1..t]]
---   [print $ solve w | w <- ws]
--- --  return ()
-main = getContents >>= print . (map solve) . tail . lines
+main = do
+  t <- readLn -- :: Int
+  mainloop t
+--  return ()
+-- mainloop :: Int -> IO ()
+mainloop 0 = return ()  
+mainloop n = do
+  w <- getLine
+  print $ solve w
+  mainloop (n - 1)
+-- main = getContents >>= print . putStrLn . (map solve) . tail . lines
 
 -- split_in_half :: [Char] -> ([Int], [Int])
--- solve :: String -> Int  
+solve :: String -> Int  
 solve cs = sum $ take (div (length cs) 2) (map abs (zipWith (-) (map ord cs) (map ord $ reverse cs)))
 -- split_in_half (ignore the middle letter in odd-lettered words)
 -- subtract first half from the second,
